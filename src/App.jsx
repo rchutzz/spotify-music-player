7e71a0454cb51f8159f1bf5a0e4f941f827b8dd9
hirichardhai <hirichardhai@gmail.com>
2018-09-23 02:06:24 -0700

    created css file and centered content

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
     render () {
         return (
-            <div>
+            <div className="App">
                 <div className="App-title">Richard's Spotify Music Player</div>
                 <div>
                     <input placeholder="search for an artist..."/>
-                    <button>button</button>
+                    <button>Submit</button>
                 </div>
                 <div className="Profile">
                     <div>Artist Picture</div>
