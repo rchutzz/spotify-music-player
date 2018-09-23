867897b7b3f901175140b2dde8ebe9b279406f69
hirichardhai <hirichardhai@gmail.com>
2018-09-23 02:02:47 -0700

    created overall structure for music player

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ class App extends Component {
     render () {
         return (
             <div>
-                Hello test
+                <div className="App-title">Richard's Spotify Music Player</div>
+                <div>
+                    <input placeholder="search for an artist..."/>
+                    <button>button</button>
+                </div>
+                <div className="Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                    Gallery
+                </div>
             </div>
         )
     }
