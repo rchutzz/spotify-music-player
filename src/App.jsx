f929207a7aa8b8bfaa066fdc7fa0dc5e517076d6
hirichardhai <hirichardhai@gmail.com>
2018-09-23 02:33:56 -0700

    created state to store formcontrol search

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('this.state', this.state);
+    }
+
     render () {
         return (
             <div className="App">
@@ -12,8 +23,10 @@ class App extends Component {
                         <FormControl
                             type="text"
                             placeholder="Search for an artist"
+                            value={this.state.query}
+                            onChange={event => {this.setState({query: event.target.value})}}
                         />
-                        <InputGroup.Addon>
+                        <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
