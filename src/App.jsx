0b73522dcf0e469c2eed3f417dd65a74574e21d6
hirichardhai <hirichardhai@gmail.com>
2018-09-23 02:10:51 -0700

    imported react-bootstrap and added formgroup, input group, formcontrol, and glyphicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component {
     render () {
         return (
             <div className="App">
                 <div className="App-title">Richard's Spotify Music Player</div>
-                <div>
-                    <input placeholder="search for an artist..."/>
-                    <button>Submit</button>
-                </div>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search for an artist"
+                        />
+                        <InputGroup.Addon>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
                 <div className="Profile">
                     <div>Artist Picture</div>
                     <div>Artist Name</div>
