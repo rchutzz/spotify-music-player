f063283c1a0431dc6c921ddb0c1543ac69c82182
hirichardhai <hirichardhai@gmail.com>
2018-09-24 05:19:49 -0700

    added gallery logic to render top 10 tracks

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
-    render() {
-        return (
-            <div>
-                test
+  render() {
+    console.log('props gallery', this.props.tracks);
+    const { tracks } = this.props;
+    return (
+      <div>
+          {tracks.map((track, k) => {
+            const trackImg = track.album.images[0].url;
+            return (
+              <div
+                key={k}
+                className="track"
+              >
+                <img
+                  src={trackImg}
+                  className="track-img"
+                  alt="track"
+                />
+                <p className="track-text">
+                  {track.name}
+                </p>
             </div>
-        )
-    }
+            )
+          })}
+      </div>
+    )
+  }
 }
 
 export default Gallery;
