33068a4cab7677891aecb690a10f41b036d01069
Richard Hai <hirichardhai@gmail.com>
2018-09-23 17:38:33 -0700

    installed dotenv and added created environemtn variables inside app.js

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,11 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+require('dotenv').config()
+
+var client_id = process.env.client_id; // Your client id
+var client_secret = process.env.client_secret; // Your secret
+var redirect_uri = process.env.redirect_uri; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
