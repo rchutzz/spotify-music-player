c0fc9524d88ae8955bd68db6af1eafb7214134ee
Richard Hai <hirichardhai@gmail.com>
2019-06-03 16:06:15 -0700

    test commit in new terminal environment

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/callback', function(req, res) {
   })
 })
 
+
 let port = process.env.PORT || 8888
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
 app.listen(port)
